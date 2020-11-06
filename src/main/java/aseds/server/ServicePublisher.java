b98d42864db1595fad7672e0801966d2daa75bc1
Ouggadi Mohammed Aymane <58845118+MohammedAyamne@users.noreply.github.com>
2020-11-06 23:32:54 +0100

    Recrification

--- src/main/java/aseds/server/ServicePublisher.java
+++ src/main/java/aseds/server/ServicePublisher.java
@@ -1,24 +1,21 @@
 package aseds.server;
 
 
-//import javax.xml.ws.Endpoint;
-//import org.apache.log4j.Logger;
-//import aseds.webservices.ProductsManagerImpl;
-
-
+import javax.xml.ws.Endpoint;
+import org.slf4j.LoggerFactory;
+import aseds.webservices.ProductsManagerImpl;
 
 public class ServicePublisher {
   
 	
-	//private static Logger logger = LoggerFactory.getLogger(ServicePublisher.class);
+	private static org.slf4j.Logger logger = LoggerFactory.getLogger(ServicePublisher.class);
 	 
 	public static void main(String[] args) {
-	//	logger.info("Démarrage du serveur");
+		logger.info("Démarrage du serveur");
 		
-	//	Endpoint.publish("http://localhost:2020/webservices/ProductManager", new ProductsManagerImpl());
+		Endpoint.publish("http://localhost:2020/webservices/ProductManager", new ProductsManagerImpl());
 	
-	//	logger.info("Serveur démarré");
-		System.out.println("salam");
+		logger.info("Serveur démarré");
 	
 	}
 	
